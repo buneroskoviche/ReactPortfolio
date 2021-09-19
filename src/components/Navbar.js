acea98985ac834b9845cd9fbac4633ed891a8864
Buney <dpkinser93@gmail.com>
2021-09-19 13:53:01 -0600

    formatted the navbar some more

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Toolbar, Button, Stack, Typography, useScrollTrigger, Slide } from '@mui/material';
+import { AppBar, Toolbar, Button, Grid, Typography, useScrollTrigger, Slide } from '@mui/material';
 
 const HideOnScroll = (props) => {
     const { children } = props;
@@ -35,32 +35,36 @@ const Navbar = () => {
         <HideOnScroll>
             <AppBar>
                 <Toolbar>
-                    <Stack direction="row" spacing={6}>
-                        <Typography variant="h4">
-                            Daniel Kinser
-                        </Typography>
-                        {
-                            navBtns.map(({href, title}, index) => {
-                                return (
-                                    <a href={href} style={{textDecoration: 'none'}}>
-                                        <Button variant="contained">{title}</Button>
-                                    </a>
-                                )
-                            })
-                        }
-                        <Button 
-                            variant="contained"
-                            onClick={() => window.open('https://github.com/buneroskoviche', '_blank')}
-                        >
-                            GitHub
-                        </Button>
-                        <Button 
-                            variant="contained"
-                            onClick={() => window.open('https://www.linkedin.com/in/danielkinser/', '_blank')}
-                        >
-                            LinkedIn
-                        </Button>
-                    </Stack>
+                    <Grid container>
+                        <Grid item xs={2} sx={{alignSelf: 'center'}}>
+                            <Typography variant="h4">
+                                Daniel Kinser
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={10} sx={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', flexWrap: 'wrap'}}>
+                            {
+                                navBtns.map(({href, title}, index) => {
+                                    return (
+                                        <a href={href} style={{textDecoration: 'none', margin: '2% 0'}}>
+                                            <Button variant="contained">{title}</Button>
+                                        </a>
+                                    )
+                                })
+                            }
+                            <Button 
+                                variant="contained"
+                                onClick={() => window.open('https://github.com/buneroskoviche', '_blank')}
+                            >
+                                GitHub
+                            </Button>
+                            <Button 
+                                variant="contained"
+                                onClick={() => window.open('https://www.linkedin.com/in/danielkinser/', '_blank')}
+                            >
+                                LinkedIn
+                            </Button>
+                        </Grid>
+                    </Grid>
                 </Toolbar>
             </AppBar>
         </HideOnScroll>
